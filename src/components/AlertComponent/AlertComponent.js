eb66479c0d422d9ff79559b00ddf0cf640af29f4
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-06 19:06:12 +0530

    Add Alert component

--- src/components/AlertComponent/AlertComponent.js
+++ src/components/AlertComponent/AlertComponent.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import './AlertComponent.css';
 
-const renderAlertMessage = (profit, loss, unChanged) => {
-    if (profit) {
-        return "Congratulations! Your stocks are making profit!🎉";
-    } else if (loss) {
-        return "Sorry! Your stocks don't seem to make profit!😭";
-    } else if (unChanged) {
+const renderAlertMessage = (profit, loss, unChanged, value, valuePercentage) => {
+    if (profit && !unChanged && !loss) {
+        return `Congratulations! Your profit is ${value} and profit percentage is ${valuePercentage}!🎉`;
+    } else if (!profit && !unChanged && loss) {
+        return `Sorry! Your loss is ${value} and loss percentage is ${valuePercentage}!😭`;
+    } else if (unChanged && !profit && !loss) {
         return "No pain no gain, no gain no pain."
     }
 }
 
 const appendStyle = (profit, loss, unChanged) => {
-    if (profit) {
+    if (profit && !unChanged && !loss) {
         return "profit";
-    } else if (loss) {
+    } else if (!profit && !unChanged && loss) {
         return "loss";
-    } else if (unChanged) {
+    } else if (unChanged && !profit && !loss) {
         return "unchanged";
+    } else {
+        return "null";
     }
 }
 
-const AlertComponent = ({profit, loss, unChanged }) => {
+const AlertComponent = ({profit, loss, unChanged, value, valuePercentage }) => {
     return (
         <div className={`alert-component-wrapper ${appendStyle(profit, loss, unChanged)}`}>
-            <p className='alert'>{renderAlertMessage(profit, loss, unChanged)}</p> 
+            <p className='alert'>{renderAlertMessage(profit, loss, unChanged, value, valuePercentage)}</p> 
         </div>
     )
 }
